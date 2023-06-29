c8775f0736f9b6be6a6a37338c3562f82e70c78b
sakusan <sssaku.039@gmail.com>
2023-06-29 10:25:16 +0000

    Update 2 files Ruter.jsxファイルを追加、RouterComponentを分ける。

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
-import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Link } from "react-router-dom";
 
-import { Home } from "./Home";
-import { Page1 } from "./Page1";
-import { Page1DetailA } from "./Page1DetailA";
-import { Page1DetailB } from "./Page1DetailB";
-import { Page2 } from "./Page2";
+import { Router } from "./router/Router";
+
 import "./styles.css";
 
 export default function App() {
@@ -17,32 +14,7 @@ export default function App() {
         <br />
         <Link to="/page2"> Page2</Link>
       </div>
-      <Switch>
-        {/* exactを入れることで完全一致。入れないと全てのpathがHomeになる */}
-        <Route exact path="/">
-          <Home />
-        </Route>
-        {/* render関数はデフォルトpropsを受け取っている。 */}
-        <Route
-          path="/page1"
-          render={({ match: { url } }) => (
-            <Switch>
-              <Route exact path={url}>
-                <Page1 />
-              </Route>
-              <Route path={`${url}/detailA`}>
-                <Page1DetailA />
-              </Route>
-              <Route path={`${url}/detailB`}>
-                <Page1DetailB />
-              </Route>
-            </Switch>
-          )}
-        />
-        <Route path="/page2">
-          <Page2 />
-        </Route>
-      </Switch>
+      <Router />
     </BrowserRouter>
   );
 }
